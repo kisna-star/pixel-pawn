33cee59c4baa26feac202f35ba9464217cb59346
kisna-star <kisna0ai@gmail.com>
2026-03-24 22:29:09 +0530

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // If your game has specific colors or animations, 
+      // you can add them here later.
+    },
+  },
+  plugins: [],
+}
